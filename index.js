28e988ded5e16c0f1e9348458762a7bee5539e95
RiP3rQ <rafalpompa2000@gmail.com>
2022-10-23 22:31:03 +0200

    feat: game time and game over fix: enemy attack

--- index.js
+++ index.js
@@ -121,6 +121,7 @@ const keys = {
     },
 }
 
+// funkcja wykrywania kolizji
 function rectangularCollision({
     rectangular1,
     rectangular2,
@@ -130,6 +131,36 @@ function rectangularCollision({
         && rectangular1.attackBox.position.y + rectangular1.attackBox.height >= rectangular2.position.y
         && rectangular1.attackBox.position.y <= rectangular2.position.y + rectangular2.height)
 }
+
+// determin winner function
+function determineWinner({ player, enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if(player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    }else if(player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player1 wins!'
+    }else{
+        document.querySelector('#displayText').innerHTML = 'Player2 wins!'
+    }
+}
+
+// timer
+let timer = 60
+let timerId
+function decreaseTimer(){
+    timerId = setTimeout(decreaseTimer, 1000)
+    if(timer > 0) {
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    if(timer == 0){
+        determineWinner({ player, enemy, timerId })
+    }
+    
+}
+decreaseTimer()
 
 //animacja gry
 function animate() {
@@ -178,6 +209,11 @@ function animate() {
         player.health -= 10
         document.querySelector('#playerHealth').style.width = player.health + '%'
     }
+
+    // end game base on health
+    if(enemy.health <= 0 || player.health <= 0){
+        determineWinner({player, enemy, timerId})
+    }
 }
 
 animate()
@@ -219,7 +255,7 @@ window.addEventListener('keydown', (event) => {
             break
         
         case 'ArrowDown':
-            enemy.isAttacking = true
+            enemy.attack()
             break
     }
 })
